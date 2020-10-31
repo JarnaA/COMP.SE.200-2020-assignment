31fa93798222d4a46a7ddd8863f98ee9fd31dc5a
Jarna Arra <jarna.arra@tuni.fi>
2020-10-31 14:35:19 +0200

    Update testCamelCase.js

--- testFolder/testCamelCase.js
+++ testFolder/testCamelCase.js
@@ -4,16 +4,16 @@ import camelCase from "../src/camelCase.js"
 const expect = chai.expect
 
 describe("CamelCase", () =>{
-    it("camelCased word won't change", () =>{
-        expect(camelCase("redBlue")).to.equal("redBlue")
-    })
-    it("undercased words will change and concatenate", () =>{
-        expect(camelCase("black white")).to.equal("blackWhite")
-    })
+    //it("camelCased word won't change", () =>{
+    //    expect(camelCase("redBlue")).to.equal("redBlue")
+    //})
+    //it("undercased words will change and concatenate", () =>{
+    //    expect(camelCase("black white")).to.equal("blackWhite")
+    //})
     it("camelCase will add redundant whitespace in front of camelCased words", () =>{
         expect(camelCase("black white")).to.equal(" blackWhite")
     })
-    it("uppercase first letter will lowercase with no other changes", () =>{
-        expect(camelCase("GreenPink")).to.equal("greenPink")
-    })
+    //it("uppercase first letter will lowercase with no other changes", () =>{
+    //    expect(camelCase("GreenPink")).to.equal("greenPink")
+    //})
 })
