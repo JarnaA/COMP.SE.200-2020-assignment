e18fe69832027bda8ef8ccf61970173d8d8c34d5
Jarna Arra <jarna.arra@tuni.fi>
2020-11-08 13:42:36 +0200

    test endsWith.js

---
+++ testFolder/testEndsWith.js
@@ -0,0 +1,37 @@
+import chai from "chai"
+import endsWith from "../src/endsWith.js"
+
+const expect = chai.expect
+
+describe("tests endsWith.js with ending substring", () =>{
+    it("test with matching single char at the end", () =>{
+        expect(endsWith("market", "t")).to.equal(true)
+    })
+    it("test with not matching single char at the end", () =>{
+        expect(endsWith("market", "a")).to.equal(false)
+    })
+    it("test with matching two char at the end", () =>{
+        expect(endsWith("market", "et")).to.equal(true)
+    })
+    it("test with not matching two char at the end", () =>{
+        expect(endsWith("market", "ea")).to.not.equal(true)
+    })
+})
+
+describe("tests endsWith.js with complete substring", () =>{
+    it("test with matching string", () =>{
+        expect(endsWith("market", "market")).to.equal(true)
+    })
+    it("test with not matching string", () =>{
+        expect(endsWith("market", "marke")).to.not.equal(true)
+    })
+})
+
+describe("tests endsWith.js with substring at certain position", () =>{
+    it("test with matching substring", () =>{
+        expect(endsWith("market", "m", 1)).to.equal(true)
+    })
+    it("test with not matching string", () =>{
+        expect(endsWith("market", "e", 1)).to.not.equal(true)
+    })
+})
