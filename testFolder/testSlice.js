8f96c7d689a5ceec03cc4413e08c67d378f57a70
Jarna Arra <jarna.arra@tuni.fi>
2020-11-14 15:53:39 +0200

    Update testSlice.js

--- testFolder/testSlice.js
+++ testFolder/testSlice.js
@@ -4,7 +4,6 @@ import slice from "../src/slice.js"
 const expect = chai.expect
 
 var array = [1, 3, 5, 7]
-var result1 = "5, 7"
 
 describe("Test slice.js with four field array", () =>{
     it("slice at index 2", () =>{
@@ -15,5 +14,32 @@ describe("Test slice.js with four field array", () =>{
     })
     it("slice at index out of bounds", () =>{
         expect(slice(array, 4)).to.have.ordered.members([])
+    })
+})
+
+describe("Test with empty parameters", () =>{
+    it("empty array", () =>{
+        expect(slice(null, 3)).to.have.ordered.members([])
+    })
+    it("empty start and end parameters", () =>{
+        expect(slice(array)).to.have.ordered.members(array)
+    })
+    it("empty start parameter", () =>{
+        expect(slice(array, null, 3)).to.have.ordered.members([1, 3, 5])
+    })
+})
+
+describe("test with negative parameters", () =>{
+    it("negative start point", () =>{
+        expect(slice(array, -2)).to.have.ordered.members([5, 7])
+    })
+    it("negative start point bigger than length of array", () =>{
+        expect(slice(array, -6)).to.have.ordered.members(array)
+    })
+    it("negative end point", () =>{
+        expect(slice(array, 0, -1)).to.have.ordered.members([1, 3, 5])
+    })
+    it("negative end point bigger than length of array", () =>{
+        expect(slice(array, 0, -5)).to.have.ordered.members([])
     })
 })
