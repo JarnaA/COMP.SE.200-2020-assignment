0f9bfe6b6d8f24347b9b1dc1a5110955ddc40e7a
Mervi Jokipii <mervi.jokipii@tuni.fi>
2020-11-26 14:43:23 +0200

    Create testGet.js

---
+++ testFolder/testGet.js
@@ -0,0 +1,20 @@
+import chai from "chai"
+import get from "../src/get.js"
+
+const expect = chai.expect
+
+describe("test get.js", () =>{
+    const basket = 
+        { 'Ruisleipä': [{'price': 1.50 }]}
+    
+
+    it("gets price from object ", () =>{
+        expect(get(basket, 'Ruisleipä[0].price')).to.equal(1.50)
+    });
+    it("gets price from object with the help of array", () =>{
+        expect(get(basket, ['Ruisleipä', '0', 'price'])).to.equal(1.50)
+    });
+    it("returns default value (0) if product not found", () =>{
+        expect(get(basket, 'Maito[0].price', '0')).to.equal('0')
+    });
+   })
