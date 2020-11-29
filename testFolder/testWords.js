fa2ab155191bd7b1117b1975a86cbe8e7fdb81b8
Jarna Arra <jarna.arra@tuni.fi>
2020-11-29 11:51:30 +0200

    Update testWords.js

--- testFolder/testWords.js
+++ testFolder/testWords.js
@@ -17,6 +17,6 @@ describe("Test words.js", () =>{
         expect(words("", /[^, ]+/g)).to.have.ordered.members([])
     })
     it("test with emtpy string with non matching pattern", () =>{
-        expect(words("doesnotcompute", /[^a-z]+/g)).to.have.ordered.members([])
+        expect(words("", undefined)).to.have.ordered.members([])
     })
 })
