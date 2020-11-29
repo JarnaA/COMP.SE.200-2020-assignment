0af3879c9733fcaf0b6e584b5542869e7f0a76a3
Jarna Arra <jarna.arra@tuni.fi>
2020-11-29 11:44:15 +0200

    Update testWords.js

--- testFolder/testWords.js
+++ testFolder/testWords.js
@@ -16,4 +16,7 @@ describe("Test words.js", () =>{
     it("test with empty string and pattern",  () =>{
         expect(words("", /[^, ]+/g)).to.have.ordered.members([])
     })
+    it("test with emtpy string with non matching pattern", () =>{
+        expect(words("doesnotcompute", /[^a-z]+/g)).to.have.ordered.members([])
+    })
 })
